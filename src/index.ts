1c7159814a8fbd06059677554b7d1192e5c26ffb
chowon442 <chowon442@gmail.com>
2025-05-17 21:15:12 +0900

    fix: Swagger UI 설정 개선 및 경로 수정 index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import oauthRouter from "./routes/oauth";
 import { googleStrategy } from "./auth.config";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import path from "path";
+
 
 const app = express();
 const port = 3000;
@@ -12,14 +14,22 @@ passport.use(googleStrategy);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user as Express.User));
 
+
 app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 // Swagger 문서 로드
-const swaggerDocument = YAML.load("./src/swagger/openapi.yaml");
+const swaggerDocument = YAML.load(path.resolve(__dirname, "./swagger/openapi.yaml"));
 
 // Swagger UI 설정
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  explorer: true,
+  customCss: '.swagger-ui .topbar { display: none }',
+  swaggerOptions: {
+    validatorUrl: null,
+    docExpansion: 'list'
+  }
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -30,3 +40,4 @@ app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running at http://0.0.0.0:${port}`);
   console.log(`Swagger UI is available at http://0.0.0.0:${port}/docs`);
 });
+
