e5a9122364b877a76107921c449ac36c8523c746
hardwoong <dew04300@naver.com>
2025-05-17 16:19:23 +0900

    feat: 빌드 파일 설정

---
+++ src/scripts/build.ts
@@ -0,0 +1,25 @@
+import esbuild from "esbuild";
+
+async function build() {
+  await esbuild.build({
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    format: "cjs",
+    outdir: "./dist",
+    outExtension: { ".js": ".cjs" },
+    platform: "node",
+    target: "node20",
+    minify: true,
+    keepNames: true,
+    external: [
+      'swagger-ui-express',
+      'yamljs',
+      'mock-aws-s3',
+      'aws-sdk',
+      'nock'
+    ],
+    loader: { '.html': 'text' }
+  }).catch(() => process.exit(1));
+}
+
+build();
