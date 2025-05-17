998fd0815a56e07a47b754a7bb853366d6eccf6d
chowon442 <85076036+chowon442@users.noreply.github.com>
2025-05-17 22:01:56 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import oauthRouter from "./routes/oauth";
 import { googleStrategy } from "./auth.config";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 passport.use(googleStrategy);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user as Express.User));
@@ -21,7 +23,7 @@ const swaggerDocument = YAML.load("./src/swagger/openapi.yaml");
 // Swagger UI 설정
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 app.use("/oauth2", oauthRouter);
