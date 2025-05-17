426a06a870a7ee3930229d93ee75635b84c5b395
hardwoong <dew04300@naver.com>
2025-05-17 17:06:27 +0900

    feat: 스웨거 추가

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import sessionMiddleware from "./utils/session";
 import passport from "passport";
 import oauthRouter from "./routes/oauth";
 import { googleStrategy } from "./auth.config";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 const app = express();
 const port = 3000;
 passport.use(googleStrategy);
@@ -12,6 +15,13 @@ passport.deserializeUser((user, done) => done(null, user as Express.User));
 app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Swagger 문서 로드
+const swaggerDocument = YAML.load("./src/swagger/openapi.yaml");
+
+// Swagger UI 설정
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -20,4 +30,5 @@ app.use("/oauth2", oauthRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Swagger UI is available at http://localhost:${port}/docs`);
 });
