d28f43c17280cdc1192a34194f8c1b3ee7af36ef
hardwoong <dew04300@naver.com>
2025-05-17 17:06:27 +0900

    feat: 스웨거 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 const port = 3000;
+
+// Swagger 문서 로드
+const swaggerDocument = YAML.load('./src/swagger/openapi.yaml');
+
+// Swagger UI 설정
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,4 +17,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Swagger UI is available at http://localhost:${port}/docs`);
 });
